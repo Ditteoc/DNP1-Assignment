879f80559d338be2a7034af3e7728bdcbe2ea732
Ditteoc <147693769+Ditteoc@users.noreply.github.com>
2024-11-01 12:22:35 +0100

    Service implementation

---
+++ Client/BlazorApp/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using DTOs;
+
+namespace BlazorApp.Services;
+
+public interface IUserService
+{
+    Task<UserDTO> AddUserAsync(CreateUserDTO request);
+    Task UpdateUserAsync(int id, UpdateUserDTO request);
+    Task<UserDTO> GetUserAsync(int id);
+    Task<IEnumerable<UserDTO>> GetUsersAsync();
+    Task DeleteUserAsync(int id);
+}
